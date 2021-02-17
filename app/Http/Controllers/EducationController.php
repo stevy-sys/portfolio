94f312e23bdf40624d8586cb6ebd7700c69e3b84
stevy-sys <62965624+stevy-sys@users.noreply.github.com>
2021-02-17 18:09:21 +0100

    installation de education dans bdd

--- app/Http/Controllers/EducationController.php
+++ app/Http/Controllers/EducationController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Education;
 use Illuminate\Http\Request;
 
 class EducationController extends Controller
 {
     public function index()
     {
-        return view('page.education');
+        $education = Education::all();
+        return view('page.education',['education' => $education]);
     }
 }
