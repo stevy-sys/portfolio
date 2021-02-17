5fae5a835189b26530a69ec32f5d66fda4fa05a0
stevy-sys <62965624+stevy-sys@users.noreply.github.com>
2021-02-17 18:45:13 +0100

    installation de experience dans bdd

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Experience;
 use Illuminate\Http\Request;
 
 class ExperienceController extends Controller
 {
     public function index()
     {
-        return view('page.experience');
+        $experience = Experience::all();
+        return view('page.experience',['experience' => $experience]);
     }
 }
