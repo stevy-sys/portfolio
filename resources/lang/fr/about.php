25a099defe91c41e372f98b4ab16f9a06b16402b
stevy-sys <62965624+stevy-sys@users.noreply.github.com>
2021-02-17 12:28:44 +0100

    affichage des texte

---
+++ resources/lang/fr/about.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+
+    'a-propos' => 'A propos de moi',
+    'qui' => 'Qui suis-je ?',
+    'date' => 'Jai appris a coder depuis 2015.',
+    'passion' => 'Je suis passionner sur le monde de programamtion informatique.',
+    'probleme' => "jaime resoudre les problemes algorithmique complexe.",
+    'autodidacte' => "jai suivie une formation professionnalisante en informatique et aussi je suis fort en autodidacte.",
+
+    
+
+];
---
+++ resources/lang/fr/home.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+
+    'bonjour' => 'Bonjour',
+    'moi' => 'Je suis Stevy . ',
+    'portfolio' => 'Bienvenue dans mon portfolio',
+    'telecharger' => 'Telecharger mon cv',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'je_suis' => 'Je suis un',
+    'metier' => 'Devellopeur full stack',
+    'voir_portfolio' => 'voir mon portfolio'
+
+];
